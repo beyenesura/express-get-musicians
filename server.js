aa1244a37742e3ccbf25cfc48773f958488b44e1
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-03-06 16:53:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const {sequelize} = require("./db")
 const port = 3000;
 
 //TODO
+app.get('/musicians', async (req, res) => {
+  try {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+  } catch (err) {
+    console.error('Error fetching musicians', err);
+    res.status(500).send('Internal server error');
+  }
+});
+
 
 app.listen(port, () => {
     sequelize.sync();
